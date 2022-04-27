6580ef243c21345fd1e962b0d307001eb4f7cb1a
fukfsa <zxcvbnm20130314@aliyun.com>
2022-04-27 10:13:22 +0800

    Update wzxy-dailyreport.py

--- wzxy-dailyreport.py
+++ wzxy-dailyreport.py
@@ -200,7 +200,7 @@ class WoZaiXiaoYuanPuncher:
             notifyToken = os.environ['SCT_KEY']
             url = "https://sctapi.ftqq.com/{}.send"
             body = {
-                "title": "⏰ 我在校园打卡结果通知",
+                "title": notifyResult,
                 "desp": "打卡项目：日检日报\n\n打卡情况：{}\n\n打卡时段：{}\n\n打卡时间：{}".format(notifyResult, notifySeq, notifyTime)
             }
             requests.post(url.format(notifyToken), data=body)
